4234c4497667f1d430c5414ba54cc4da555804f1
Fernando Rodriguez <fernando@nandotech.com>
2016-03-06 14:14:59 -0500

    Working code with assemblies included

---
+++ Contacts/Contact.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contacts
+{
+    public partial class Contact : Form
+    {
+        public Contact()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact List 1.0.\nWritten By: Fernando Rodriguez", "About");
+            
+
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet1);
+
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'contactDBDataSet1.People' table. You can move, or remove it, as needed.
+            this.peopleTableAdapter.Fill(this.contactDBDataSet1.People);
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
